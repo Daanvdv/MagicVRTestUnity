10bcb8bdd9ef23deec2675a0f610bb8f6e580d48
Daanvdv <35851026+Daanvdv@users.noreply.github.com>
2019-10-21 17:26:36 +0200

    Added Basic Golem Script

---
+++ Assets/Scripts/GolemScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class GolemScript : MonoBehaviour
+{
+    //SteamVR input
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean trigger;
+    public SteamVR_Action_Boolean menu;
+    public SteamVR_Action_Boolean grip;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Checks if trigger is pressed
+    bool GetTrigger()
+    {
+        return trigger.GetState(handType);
+    }
+
+    //Checks if the grip is pressed
+    bool GetGrip()
+    {
+        return grip.GetState(handType);
+    }
+
+    //Checks if menu is pressed
+    bool GetMenu()
+    {
+        return menu.GetState(handType);
+    }
+}
