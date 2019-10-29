d14f9c057ee00495f6042995385407dfc2a3e72c
Daanvdv <35851026+Daanvdv@users.noreply.github.com>
2019-10-29 19:13:12 +0100

    Create DelaySoundScript.cs

---
+++ Assets/Scripts/DelaySoundScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelaySoundScript : MonoBehaviour
+{
+    public AK.Wwise.Event sound;
+    public float minRange;
+    public float maxRange;
+
+    private float timer = 0.0f;
+    private float timeToPlaySound;
+    private bool timerDone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToPlaySound = Random.Range(0.2f, 15.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timeToPlaySound >= timer && !timerDone)
+        {
+            PlaySound();
+        }
+    }
+
+    void PlaySound()
+    {
+        sound.Post(this.gameObject);
+        timerDone = true;
+    }
+}
