d6086e45ecdddcdd44d4d9d78c9b6ed9acf13cb1
Daanvdv <vandeven.daan@gmail.com>
2019-11-28 16:51:36 +0100

    Basic interactable test

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    HandScript hand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.TryGetComponent<HandScript>(out hand);
+
+        hand.objectColliding = this;
+    }
+
+    public void PickUpObject(GameObject other)
+    {
+        this.transform.parent = other.transform;
+    }
+
+    public void DropObject()
+    {
+        this.transform.parent = null;
+    }
+}
