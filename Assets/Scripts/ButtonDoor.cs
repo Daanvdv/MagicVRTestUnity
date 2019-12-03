22696b30bd98beafd26e53ce627245323cec13ed
Daanvdv <vandeven.daan@gmail.com>
2019-12-03 21:49:40 +0100

    Added button door script

---
+++ Assets/Scripts/ButtonDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDoor : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+
+    public bool allowMoveBack = true;
+    public float acceptanceValue = 1.0f;
+
+    public float moveSpeed = 1.5f;
+
+    private bool openDoor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openDoor)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, endPoint.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, startPoint.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        openDoor = true;
+    }
+
+    public void CloseDoor()
+    {
+        openDoor = false;
+    }
+}
