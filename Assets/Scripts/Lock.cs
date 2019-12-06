d770723ddc5e3acb1203a011520f3c0748323b30
Daanvdv <35851026+Daanvdv@users.noreply.github.com>
2019-12-06 14:05:46 +0100

    Lock script

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Lock : MonoBehaviour
+{
+    public Transform keyinLockLocation;
+    public GameObject key;
+
+    private Key keyScript;
+
+    /// Awake is called when the script instance is being loaded.
+    /*void Awake()
+    {
+        if (key.TryGetComponent<Key>(out keyScript))
+            this.enabled = false;
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject interactable = other.gameObject.transform.parent.gameObject;
+        if (interactable  == key)
+        {
+            interactable.transform.parent = keyinLockLocation;
+            interactable.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+}
