d0690f12a84f66159bf89a67065e4d3c6eb3245e
Daanvdv <35851026+Daanvdv@users.noreply.github.com>
2019-12-13 13:54:00 +0100

    Update Breakable.cs

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -1,36 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Valve.VR.InteractionSystem;
 
-namespace Valve.VR.InteractionSystem.Sample
+public class Breakable : MonoBehaviour
 {
-    public class Breakable : MonoBehaviour
+    public GameObject[] brokenPieces;
+    public float minMagnitudeToBreak = 1.0f;
+    
+    //Audio
+    public AK.Wwise.Event breakEvent;
+
+    private Interactable interactable;
+
+    private void Start()
     {
-        //public GameObject explodePartPrefab;
-        //public int explodeCount = 10;
+        interactable = this.GetComponent<Interactable>();
+    }
 
-        public float minMagnitudeToExplode = 10f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (interactable != null && interactable.attachedToHand != null) //don't explode in hand
+            return;
 
-        private Interactable interactable;
-
-        private void Start()
+        if (collision.impulse.magnitude > minMagnitudeToBreak)
         {
-            interactable = this.GetComponent<Interactable>();
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (interactable != null && interactable.attachedToHand != null) //don't explode in hand
-                return;
-
-            if (collision.impulse.magnitude > minMagnitudeToExplode)
+            breakEvent.Post(this.gameObject);
+            this.gameObject.GetComponent<MeshRenderer>();
+            foreach (GameObject piece in brokenPieces)
             {
-                //for (int explodeIndex = 0; explodeIndex < explodeCount; explodeIndex++)
-                //{
-                //    GameObject explodePart = (GameObject)GameObject.Instantiate(explodePartPrefab, this.transform.position, this.transform.rotation);
-                //    explodePart.GetComponentInChildren<MeshRenderer>().material.SetColor("_TintColor", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
-                //}
-
-                Destroy(this.gameObject);
+                Instantiate(piece, this.transform);
+                piece.gameObject.AddComponent<Rigidbody>();
             }
         }
     }
